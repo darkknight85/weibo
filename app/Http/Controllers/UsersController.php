37eca5d1e911aad48a7dfb70aa28b12c19a9b6cf
vagrant <vagrant@homestead>
2020-09-10 01:48:03 +0000

    中文的错误提示信息

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -19,13 +19,23 @@ class UsersController extends Controller
         return view('users.show',compact('user'));
     }
 
+    // public function store(Request $request)
+    // {
+    //     $this->validate($request, [
+    //         'name' => 'required|unique:users|max:50',
+    //         'email' => 'required|email|unique:users|max:255',
+    //         'password' => 'required|confirmed|min:6'
+    //     ]);
+    //     return;
+    // }
+
     //store方法
     public function store(Request $request)
     {
         $this->validate($request, [
             'name'=>'required|unique:users|max:50', //存在性验证，唯一性验证，长度验证
             'email'=>'required|email|unique:users|max:255',//存在，唯一，邮件格式，长度验证
-            'password'=>'require|confirmed|min:6' //存在，重复性，长度验证
+            'password'=>'required|confirmed|min:6' //存在，重复性，长度验证
         ]);
         return;
     }
