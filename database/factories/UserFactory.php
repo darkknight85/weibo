a29cf93573e65de265199397c1aa3d5c6c9df28f
vagrant <vagrant@homestead>
2020-09-17 02:24:55 +0000

    社交统计信息

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ $factory->define(User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'activated' => true,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => '$2y$10$yCaJt5kFS91FXcg4nTl1d.2Are56q7DeGB75QXmqw3qPVAFh4ixVO', // secret
         'remember_token' => Str::random(10),
         'created_at' => $date_time,
         'updated_at' => $date_time,
